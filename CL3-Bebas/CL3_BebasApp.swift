0b422f56f005d6df311e699695415712335efa5c
rafaelsitorus <rafaelraja.sitorus@gmail.com>
2026-05-26 14:32:36 +0700

    Initial Commit

---
+++ CL3-Bebas/CL3_BebasApp.swift
@@ -0,0 +1,17 @@
+//
+//  CL3_BebasApp.swift
+//  CL3-Bebas
+//
+//  Created by Pangihutan Sitorus on 26/05/26.
+//
+
+import SwiftUI
+
+@main
+struct CL3_BebasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CL3-Bebas/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CL3-Bebas
+//
+//  Created by Pangihutan Sitorus on 26/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

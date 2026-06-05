040437b2d8b4a923e53ff3c3cd94a04784d27865
rafaelsitorus <rafaelraja.sitorus@gmail.com>
2026-06-05 14:31:54 +0700

    feat: Extension Design Token

---
+++ CL3-Bebas/Core/UI/Color+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  DesignToken.swift
+//  CL3-Bebas
+//
+//  Created by Pangihutan Sitorus on 05/06/26.
+//
+
+import SwiftUI
+
+extension Color {
+    static let RedPrimaryBC = Color("#2992E9")
+    static let RedSecondaryBC = Color("#BFE2FF")
+    static let RedAccentBC = Color("#F6FDFF")
+    
+    static let RedPrimarySC = Color("#D33838")
+    static let RedSecondarySC = Color("#FFE6E6")
+    static let GreyAccentSC = Color("#979797")
+}

675c1d0f519403dc8f85039404941e77b8101ece
Theona77 <theonaarlinton.dy@gmail.com>
2026-06-05 14:54:19 +0700

    feat: Radius Border Token

---
+++ CL3-Bebas/Core/UI/DesignToken/Radius+Enum.swift
@@ -0,0 +1,13 @@
+//
+//  Radius+Enum.swift
+//  CL3-Bebas
+//
+//  Created by Theona Arlinton on 05/06/26.
+//
+
+import Foundation
+
+enum Radius {
+    static let MainCard: CGFloat = 20
+    static let smallCard: CGFloat = 10
+}

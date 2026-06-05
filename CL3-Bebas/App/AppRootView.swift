961c4b6a9e76d73b0eb8e024314cfae365465ff1
rafaelsitorus <rafaelraja.sitorus@gmail.com>
2026-06-05 14:11:56 +0700

    feat: restore boilerplate and work progress

--- CL3-Bebas/App/AppRootView.swift
+++ CL3-Bebas/App/AppRootView.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 struct AppRootView: View {
     var body: some View {
-        HomeView(viewModel: HomeViewModel())
+        
     }
 }
 
--- CL3-Bebas/Features/Home/ViewModels/HomeViewModel.swift
+++
@@ -1,5 +0,0 @@
-import Foundation
-
-@MainActor
-final class HomeViewModel: ObservableObject {
-}

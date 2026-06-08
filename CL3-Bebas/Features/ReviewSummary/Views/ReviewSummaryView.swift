24e727112010122c36e90df0cc8276b417fc39b7
Theona77 <theonaarlinton.dy@gmail.com>
2026-06-08 22:54:52 +0700

    ReviewSummaryView

---
+++ CL3-Bebas/Features/ReviewSummary/Views/ReviewSummaryView.swift
@@ -0,0 +1,24 @@
+//
+//  ReviewSummaryView.swift
+//  CL3-Bebas
+//
+//  Created by Theona Arlinton on 08/06/26.
+//
+
+//
+import SwiftUI
+
+struct ReviewSummaryView: View {
+    let onContinue: () -> Void
+    let onSkip: () -> Void
+
+    var body: some View {
+        VStack(spacing: 0) {
+        }
+          
+    }
+}
+
+#Preview {
+    ReviewSummaryView()
+}
